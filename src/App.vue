b41671be1e4fd5afeedf096d43d0b574903db63b
DrebinPeng <924677591@qq.com>
2021-04-03 22:16:43 +0800

    refactor: clean other useless files

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,63 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+  <div class="editor-container">
+    <div class="editor" ref="editor"></div>
   </div>
 </template>
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator'
+;import * as monaco from 'monaco-editor/esm/vs/editor/editor.api';
+import * as ts from 'typescript';
+import { browser } from 'webextension-polyfill-ts';
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
+@Component
+export default class App extends Vue {
+  private txt = 'Hello World 123 typescript';
+  private monacoEditor = null;
+  private originCode = 'console.log(123123)';
+  private mounted() {
+    this.monacoEditor = monaco.editor.create((this.$refs as any).editor, {
+      value: this.originCode,
+      language: 'typescript',
+      theme: 'vs-dark',
+    });
+    this.monacoEditor.onDidChangeModelContent(() => {
+      this.change(this.monacoEditor.getValue());
+    });
+    this.monacoEditor.addCommand(monaco.KeyMod.CtrlCmd | monaco.KeyCode.KEY_S, () => {
+      this.runCode();
+    });
+    this.registerResizeListener();
   }
+  private beforeDestroy() {
+    this.removeResizeListener();
+  }
+  private change(value: string) {
+    this.originCode = value;
+  }
+  private runCode() {
+    const result = ts.transpileModule(this.originCode, { compilerOptions: { module: ts.ModuleKind.CommonJS } });
+    console.log(result)
+    // browser.devtools.inspectedWindow.eval(result.outputText);
+  }
+  private registerResizeListener() {
+    window.addEventListener('resize', this.resizeEditor.bind(this));
+  }
+  private removeResizeListener() {
+    window.removeEventListener('resize', this.resizeEditor.bind(this));
+  }
+  private resizeEditor() {
+    this.monacoEditor.layout()
+  }
+}
+</script>
+<style lang="scss">
+.editor-container {
+  height:  100vh;
+  width: 100vw;
+  background: red;
+}
+.editor {
+  height: 100%;
+  width: 100%;
 }
 </style>
